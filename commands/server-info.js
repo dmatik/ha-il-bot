f4b0112b54dc0f6acf602120850b4f749bfa6def
dmatik <dmatik@gmail.com>
2020-04-30 06:16:47 +0300

    fix on server-info

--- commands/server-info.js
+++ commands/server-info.js
@@ -12,15 +12,16 @@ module.exports = {
 
 			const embed = new Discord.MessageEmbed()
 				.addFields(
-					{ name: 'Owner', value: message.guild.owner.user.username, inline: true },
+					{ name: 'Owner', value: message.guild.owner.user.tag, inline: true },
 					{ name: 'Server Created', value: message.guild.createdAt.getDate() + '/' + (message.guild.createdAt.getMonth() + 1) + '/' + message.guild.createdAt.getFullYear(), inline: true },
-					{ name: 'Roles', value: message.guild.roles.holds.length, inline: true },
+					// { name: 'Roles', value: message.guild.roles.holds.length, inline: true },
 					{ name: 'Memebers', value: message.guild.memberCount + ' members\n' + totalOnline.size + ' online\n', inline: true },
-					{ name: 'Channels', value: message.guild.channels.holds.length, inline: true },
+					// { name: 'Channels', value: totalChannels.size, inline: true },
 				)
-				.setFooter(`Server name: ${message.guild.name}`);
+				.setFooter(`Server Name: ${message.guild.name}`);
 
 			message.channel.send(embed);
+
 		});
 
 	},
