9de283ecb5bf020e8c276535393859bce320fa58
dmatik <dmatik@gmail.com>
2020-05-02 09:00:56 +0300

    enchancing user-info command

--- commands/server-info.js
+++ commands/server-info.js
@@ -7,25 +7,32 @@ module.exports = {
 	cooldown: 5,
 	execute(message, args) {
 
-		message.guild.members.fetch().then(fetchedMembers => {
-			const totalOnline = fetchedMembers.filter(member => member.presence.status === 'online');
+		const totalOnlineMembers = message.guild.members.cache.filter((m) => m.presence.status != 'offline').size;
 
-			const embed = new Discord.MessageEmbed()
-				.addFields(
-					{ name: 'Owner', value: message.guild.owner.user.tag, inline: true },
-					{ name: 'Server Created', value: message.guild.createdAt.getDate()
-						+ '/' + (message.guild.createdAt.getMonth() + 1)
-						+ '/' + message.guild.createdAt.getFullYear(),
-					inline: true },
-					// { name: 'Roles', value: message.guild.roles.holds.length, inline: true },
-					{ name: 'Memebers', value: message.guild.memberCount + ' members\n' + totalOnline.size + ' online\n', inline: true },
-					// { name: 'Channels', value: totalChannels.size, inline: true },
-				)
-				.setFooter(`Server Name: ${message.guild.name}`);
+		const totalTextChannels = message.guild.channels.cache.filter((c) => c.type === 'text').size;
+		const totalVoiceChannels = message.guild.channels.cache.filter((c) => c.type === 'voice').size;
+		const totalCategories = message.guild.channels.cache.filter((c) => c.type === 'category').size;
 
-			message.channel.send(embed);
+		const embed = new Discord.MessageEmbed()
+			.addFields(
+				{ name: 'Owner', value: message.guild.owner.user.tag, inline: true },
+				{ name: 'Server Created', value: message.guild.createdAt.getDate()
+					+ '/' + (message.guild.createdAt.getMonth() + 1)
+					+ '/' + message.guild.createdAt.getFullYear(),
+				inline: true },
+				{ name: 'Roles', value: message.guild.roles.cache.size, inline: true },
+				{ name: 'Memebers', value: message.guild.memberCount + ' members\n'
+					+ totalOnlineMembers + ' online\n', inline: true },
+				{ name: 'Channels', value: message.guild.channels.cache.size + ' channels:\n'
+					+ totalCategories + ' categories\n'
+					+ totalTextChannels + ' text, '
+					+ totalVoiceChannels + ' voice',
+				inline: true },
+			)
+			.setThumbnail(message.guild.iconURL({ format: 'png', dynamic: true }))
+			.setFooter(`Server Name: ${message.guild.name}`);
 
-		});
+		message.channel.send(embed);
 
 	},
 };
